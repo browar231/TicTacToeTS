e383b5f62fbb6708eeeaa9d3175690a97313cfec
pawelbo231 <pawelbo231@gmail.com>
2023-12-29 23:34:58 +0100

    Game::isGameWon

--- ts/Game.ts
+++ ts/Game.ts
@@ -4,6 +4,10 @@ export class Game {
     private players: Player[];
     private board: Board;
     constructor(player1: Player, player2: Player) {
+        this.players = [player1, player2]
         this.board = new Board;
+    }
+    isGameWon() {
+        return this.board.isGameWon();
     }
 }
