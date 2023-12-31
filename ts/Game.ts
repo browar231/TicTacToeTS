e48f7c59c04d82d3f51659b8d58a337e9fde49ce
pawelbo231 <pawelbo231@gmail.com>
2023-12-31 11:31:44 +0100

    game initialization

--- ts/Game.ts
+++ ts/Game.ts
@@ -2,7 +2,8 @@ import { Player } from './Players.js'
 import { Board } from './Board.js'
 export class Game {
     private players: Player[];
-    private board: Board;
+    private board: Board = new Board;
+    private currentPlayerIndex: number = 0;
     constructor(player1: Player, player2: Player) {
         this.players = [player1, player2]
         this.board = new Board;
