7292da6c9d69008d743f8c073a1c0a2f44f20099
pawelbo231 <pawelbo231@gmail.com>
2023-12-31 11:30:39 +0100

    abstract class Player meant to be extended as human or cpu player

--- ts/Players.ts
+++ ts/Players.ts
@@ -1,3 +1,12 @@
-export class Player {
-
+import { Board } from './Board.js'
+export abstract class Player {
+    private name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    abstract provideField(board: Board): number;
+    returnName() {
+        return this.name;
+    }
+}
 }
