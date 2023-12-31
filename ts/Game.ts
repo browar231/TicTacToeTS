b701f0a643ddda1152b52f03f2e8f35afeaae41c
pawelbo231 <pawelbo231@gmail.com>
2023-12-31 11:33:36 +0100

    Game::play game loop

--- ts/Game.ts
+++ ts/Game.ts
@@ -6,6 +6,21 @@ export class Game {
     private currentPlayerIndex: number = 0;
     constructor(player1: Player, player2: Player) {
         this.players = [player1, player2];
+
+    }
+    async play() {
+        while (true) {
+            const fieldInput = await this.returnCurrentPlayer().provideField(this.board);
+            if (!this.board.takeField(fieldInput, this.returnCurrentPlayer())) {
+                continue;
+            }
+            if (this.isGameWon()) {
+                break;
+            }
+            this.switchToNextPlayer();
+        }
+        return this.returnCurrentPlayer();
+    }
     returnCurrentPlayer(): Player {
         return this.players[this.currentPlayerIndex];
     }
