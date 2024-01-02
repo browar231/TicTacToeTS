85e3022b48f09239108d43686bc1fa2f0ee8567a
pawelbo231 <pawelbo231@gmail.com>
2024-01-02 11:28:53 +0100

    starting game with both cpu players

--- ts/App.ts
+++ ts/App.ts
@@ -1,8 +1,8 @@
 import { Game } from './Game.js'
-import { Player } from './Players.js';
+import { Player, PlayerCPU } from './Players.js';
 export class App {
-    private player1 = new Player;
-    private player2 = new Player;
+    private player1 = new PlayerCPU('cpu #1');
+    private player2 = new PlayerCPU('cpu #2');
     private game = new Game(this.player1, this.player2);
     constructor(target: HTMLElement) {
         while (!this.game.isGameWon()) {
